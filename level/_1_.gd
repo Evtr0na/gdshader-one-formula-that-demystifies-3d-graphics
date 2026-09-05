c704d093d2a965d5abeea0055d1c885102de6797
Evtr0na <evtrona@gmail.com>
2026-09-05 13:30:17 +0800

    2

--- level/_1_.gd
+++ level/_1_.gd
@@ -10,13 +10,18 @@ var material_ := self.material as ShaderMaterial
 
 func _physics_process(_delta: float) -> void:
 	var points:PackedVector2Array = [
-		screen(project(Vector3(1.0,1.0,1.0))),
-		Vector2(0.5,0.5)
-		# screen(project(Vector3(-0.5,-0.5,1.0))),
+		screen(project(Vector3(0.5,-0.5,2.0))),
+		screen(project(Vector3(0.5,-0.5,3.0))),
+		screen(project(Vector3(0.5,0.5,2.0))),
+		screen(project(Vector3(0.5,0.5,3.0))),
+		screen(project(Vector3(-0.5,-0.5,2.0))),
+		screen(project(Vector3(-0.5,-0.5,3.0))),
+		screen(project(Vector3(-0.5,0.5,2.0))),
+		screen(project(Vector3(-0.5,0.5,3.0))),
+		# screen(project(Vector3(0.0,0.0,1.0))),
 
 		]
 	print(screen(project(Vector3(1.0,1.0,1.0))))
-	# print(screen(project(Vector3(1.0,1.0,1.0)))==Vector2(0.5,0.5))
 	material_.set_shader_parameter("points",points)
 	material_.set_shader_parameter("point_count",points.size())
 
@@ -27,8 +32,8 @@ func project(point:Vector3)->Vector2:
 	return Vector2(x_,y_)
 
 func screen(p:Vector2)->Vector2:
-	var x_ = p.x-0.5
-	var y_ = -( p.y-0.5 )
+	var x_ = p.x+0.5
+	var y_ = -p.y+0.5
 	# var x_ = p.x/2.0+0.5
 	# var y_ =1- (p.y/2.0+0.5 )
 	# print(Vector2( x_,y_) )
