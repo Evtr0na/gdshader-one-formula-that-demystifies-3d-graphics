0722ae8eef4b23d91e818555852089ef004a5536
Evtr0na <evtrona@gmail.com>
2026-09-05 23:53:53 +0800

    fix drawing issues cased by uv scaling

--- level/_1_.gd
+++ level/_1_.gd
@@ -5,10 +5,7 @@ var int_width :float
 var int_higth :float
 @export var offest:float = 1.0
 @export var radius = 0.0
-# @export var p_1:float = 1.0
-# @export var p_2:float = 1.0
-# @export var p_3:float = 1.0
-# @export var p_4:float = 1.0
+@export var smooth_values := 0.005
 
 var material_ := self.material as ShaderMaterial
 
@@ -16,6 +13,8 @@ var material_ := self.material as ShaderMaterial
 func _physics_process(_delta: float) -> void:
 	offest_()
 	var points:PackedVector2Array = [
+		screen(project(Vector3(0.0,0.0,1.0))),
+		# screen(project(Vector3(1000,500,1.0))),
 		screen(project(Vector3(0.5,-0.5,2.0))),
 		screen(project(Vector3(0.5,-0.5,3.0))),
 		screen(project(Vector3(0.5,0.5,2.0))),
@@ -24,18 +23,17 @@ func _physics_process(_delta: float) -> void:
 		screen(project(Vector3(-0.5,-0.5,3.0))),
 		screen(project(Vector3(-0.5,0.5,2.0))),
 		screen(project(Vector3(-0.5,0.5,3.0))),
-		# screen(project(Vector3(0.0,0.0,1.0))),
-
 		]
 	material_.set_shader_parameter("points",points)
 	material_.set_shader_parameter("point_count",points.size())
 	material_.set_shader_parameter("game_width",int_width)
 	material_.set_shader_parameter("game_higth",int_higth)
 	material_.set_shader_parameter("radius",radius)
+	material_.set_shader_parameter("smooth_values",smooth_values)
 
 func offest_()->void:
-	int_width = game_width*offest
-	int_higth = game_higth*offest
+	int_width = size.x*offest
+	int_higth = size.y*offest
 	
 
 
@@ -45,9 +43,10 @@ func project(point:Vector3)->Vector2:
 	return Vector2(x_,y_)
 
 func screen(p:Vector2)->Vector2:
-	var x_ = p.x+0.5*int_width
-	var y_ = -p.y+0.5*int_higth
-	# var x_ = p.x/2.0+0.5
-	# var y_ =1- (p.y/2.0+0.5 )
-	# print(Vector2( x_,y_) )
+	var x_ = (p.x+0.5)
+	var y_ = (-p.y+0.5)
 	return Vector2(x_,y_)
+
+func frame()->void
+	
+	pass
